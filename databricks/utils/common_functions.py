e9572fc73e5c396b4a140b488756e8296a81ebd2
markdev063@gmail.com <markdev063@gmail.com>
2023-10-29 07:21:44 +0000

    Account Dimension Processing

--- databricks/utils/common_functions.py
+++ databricks/utils/common_functions.py
@@ -10,5 +10,27 @@ def add_ingestion_date(df: DataFrame) -> DataFrame:
 
 # COMMAND ----------
 
+def add_surrogate_key(*fields, df: DataFrame, key_name: str) -> DataFrame:
+    return df.withColumn(key_name, F.sha2(F.concat(*fields), numBits=256))
+
+# COMMAND ----------
+
+def overwrite_table(df: DataFrame, save_path: str, partition_fields: list[str]):
+    if partition_fields:
+        (df.write
+        .format("delta")
+        .partitionBy(*partition_fields)
+        .option("path", save_path)
+        .mode("overwrite")
+        .save())
+    else:
+        (df.write
+        .format("delta")
+        .option("path", save_path)
+        .mode("overwrite")
+        .save())
+
+# COMMAND ----------
+
 def show_null_counts(df: DataFrame):
     display(df.select([F.count(F.when(F.isnotnull(c), F.col(c))).alias(c) for c in df.columns]))
